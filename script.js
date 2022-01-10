a97ca5d15c1ff2601fea80a8b0f7d8525afaadca
SilversterAlone <97253739+SilversterAlone@users.noreply.github.com>
2022-01-10 17:38:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-
-function updateRate(){
-    var rateval=document.getElementById("rate").value;document.getElementById("rate_val").innerHTML=rateval;
-}
-function checkdata(){
-    event.preventDefault();
-    var principal=document.getElementById("principal");
-    //Check if principal has 0 or negative input
-        if(principal.value <= 0){
-        alert("Enter a positive number");
-        principal.focus();
-        return false;
-    //If all is well return true
-    } return true;
-} 
-function compute(){
+function compute()
+{
     // References to the point elements we wish to validate
-    var principal=document.getElementById("principal").value
+    var principal=document.getElementById("principal").value;
     var rate=document.getElementById("rate").value;
     var years=document.getElementById("years").value;
-    var years=new Date().getFullYear()+parseInt(years);
-    var interest=principal*years*rate/100;
-    
-    // Displaying interest rate after calculations 
-    var int=document.getElementById("int").value;document.getElementById("int").innerHTML=interest;
-    
+    var year=new Date().getFullYear()+parseInt(years);
+    var interest=principal*years*rate/100; 
+    if(principal > 0 )
+    {
+        document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+",\</mark\> \<br>at an interest rate of \<mark\>"+rate+"% \</mark\>\ <br> You will receive an amount of \<mark\>"+interest+"\</mark\>\, <br> in the year \<mark\>"+year+" \</mark\>\ <br>."
+    } else 
+    {
+        alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+    }
     // Results with explanation of ever input used
-    var result=document.getElementById("result").value;document.getElementById("result").innerHTML="If you deposit "+principal+", <br>at an interest rate of "+rate+"% <br> You will receive an amount of "+interest+", <br> in the year "+years+" <br>";
+}
+function updateRate(){
+    var rateval=document.getElementById("rate").value;document.getElementById("rate_val").innerHTML=rateval;
 }
